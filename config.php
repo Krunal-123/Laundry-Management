8c37a74928e1a3b47f04a3184ad756f5c53f9fac
Krunal-Parmar <100572810+Krunal-123@users.noreply.github.com>
2024-05-26 01:12:11 +0530

    update 1.0

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
-$conn = mysqli_connect("localhost", "root", "", "laundry");
+$conn = mysqli_connect("sql12.freesqldatabase.com", "sql12709268", "alFYknE3Ul", "sql12709268");
 $_SESSION['EP']="gdej vqtm uuem qxuj";
